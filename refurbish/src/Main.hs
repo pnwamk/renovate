0cfdeaf14fbf6129882ed45f192c02c84896536d
Tristan Ravitch <tristan@nochair.net>
2018-08-29 09:31:25 -0700

    Fill out the implementation of refurbish

--- refurbish/src/Main.hs
+++ refurbish/src/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ElfEdit as E
+import           Data.Functor.Const ( Const(..) )
 import qualified Options.Applicative as O
 import qualified System.Exit as IO
 
@@ -42,7 +43,7 @@ main = O.execParser optParser >>= mainWithOptions
 mainWithOptions :: Options -> IO ()
 mainWithOptions o = do
   bytes <- BS.readFile (oInput o)
-  let configs :: [(R.Architecture, R.SomeConfig R.TrivialConfigConstraint Maybe)]
+  let configs :: [(R.Architecture, R.SomeConfig R.TrivialConfigConstraint (Const ()))]
       configs = [ (R.PPC32, R.SomeConfig (NR.knownNat @32) MBL.Elf32Repr (RP.config32 analyze rewrite))
                 , (R.PPC64, R.SomeConfig (NR.knownNat @64) MBL.Elf64Repr (RP.config64 analyze rewrite))
                 , (R.X86_64, R.SomeConfig (NR.knownNat @64) MBL.Elf64Repr (RX.config analyze rewrite))
@@ -66,11 +67,11 @@ mainWithOptions o = do
         (e', _, _) <- R.rewriteElf rc e loadedBinary R.Parallel
         LBS.writeFile (oOutput o) (E.renderElf e')
 
-analyze :: R.AnalyzeEnv arch -> MBL.LoadedBinary arch binFmt -> IO (b arch)
-analyze = undefined
+analyze :: R.AnalyzeEnv arch -> MBL.LoadedBinary arch binFmt -> IO (Const () arch)
+analyze _ _ = return (Const ())
 
-rewrite :: b arch
+rewrite :: Const () arch
         -> MBL.LoadedBinary arch binFmt
         -> R.SymbolicBlock arch
         -> R.RewriteM arch (Maybe [R.TaggedInstruction arch (R.InstructionAnnotation arch)])
-rewrite = undefined
+rewrite _ _ b = return (Just (R.basicBlockInstructions b))
